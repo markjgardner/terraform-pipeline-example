8d00b45bfa238f80c0756e678f4391eada6ffba2
Mark Gardner <mark.gardner@microsoft.com>
2019-08-22 09:01:23 -0400

    more valuable tests

--- tests/PlanTests.cs
+++ tests/PlanTests.cs
@@ -25,22 +25,22 @@ namespace tests {
 
     [Fact]
     public void Test1 () {
-      var r = resources.Single(p => p.name=="rg");
+      var r = resources.Single(p => p.name=="vnet");
       Assert.Equal("eastus2", r.change.after.location.Value);
     }
 
     [Fact]
     public void Test2()
     {
-      var r = resources.Single(p => p.name == "app-sn");
-      Assert.Equal("10.0.0.32/27", r.change.after.address_prefix.Value);
+      var r = resources.Single(p => p.name == "gw-sn");
+      Assert.Equal("10.0.0.0/27", r.change.after.address_prefix.Value);
     }
 
     [Fact]
     public void Test3()
     {
-      var r = resources.Single(p => p.name == "vnet");
-      Assert.Equal("10.0.0.0/20", r.change.after.address_space[0].Value);
+      var r = resources.Single(p => p.name == "app-sn");
+      Assert.Equal("10.0.0.32/27", r.change.after.address_prefix.Value);
     }
   }
 }
